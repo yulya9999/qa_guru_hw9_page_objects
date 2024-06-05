0a87d0706623916cea6c269eb20bdb9c0b77ba1d
Yulia.Vdovina <Yulia.Vdovina@infotecs.ru>
2024-06-05 11:05:05 +0300

    main

---
+++ pages/registration_page.py
@@ -0,0 +1,113 @@
+from selene import browser, be, have, command
+import os
+import tests
+
+
+class RegistrationPage:
+    def __init__(self):
+
+        self.state = browser.element('#state')
+        self.city = browser.element('#city')
+
+    def open_browser(self):
+
+        browser.open("/automation-practice-form")
+
+    def fill_first_name(self, value):
+
+        browser.element("#firstName").type(value)
+        return self
+
+    def fill_last_name(self, value):
+
+        browser.element("#lastName").type(value)
+        return self
+
+    def fill_email(self, value):
+
+        browser.element("#userEmail").type(value)
+        return self
+
+    def fill_number(self, value):
+
+        browser.element("#userNumber").type(value)
+        return self
+
+    def fill_address(self, value):
+
+        browser.element("#currentAddress").type(value)
+        return self
+
+    def fill_date_of_birth(self, month, day, year):
+
+        browser.element("#dateOfBirthInput").click()
+        browser.element(f'.react-datepicker__month-select option[value="{month}"]').click()
+        browser.element(f'.react-datepicker__year-select option[value="{year}"]').click()
+        browser.all(f'.react-datepicker__day--{day}').second.click()
+        return self
+
+    def fill_subject(self, value):
+
+        browser.element("#subjectsInput").type(f"{value}")
+        browser.element(".subjects-auto-complete__menu").click()
+        browser.element(
+            ".subjects-auto-complete__indicators .subjects-auto-complete__clear-indicator"
+        ).click()
+        browser.element("#subjectsInput").should(be.blank)
+        browser.element("#subjectsInput").type(f"{value}").press_tab()
+
+        return self
+
+    def fill_gender(self, value):
+
+        browser.element(f'[for="gender-radio-{value}"]').click()
+        return self
+
+    def fill_hobbies(self, value):
+
+        browser.element('[for="hobbies-checkbox-1"]').click()
+        return self
+
+    def upload_file(self, file_name):
+
+        browser.element(".form-file").click().element("#uploadPicture").send_keys(
+            os.path.abspath(f"data/{file_name}")
+        )
+        return self
+
+    def fill_state(self, value):
+        self.state.perform(command.js.scroll_into_view).click()
+        self.state.all('[id^=react-select-3-option]').element_by(
+            have.text(value)
+        ).click()
+
+        return self
+
+    def fill_city(self, value):
+        self.city.click()
+        self.city.all('[id^=react-select-4-option]').element_by(
+            have.text(value)
+        ).click()
+
+        return self
+
+    def submit(self):
+        browser.element("#submit").click()
+        return self
+
+    def registered_user_with(self, full_name, email, gender, phone, date, subject, hobbies, file, address,
+                             state):
+        browser.element(".modal-content").element("table").all("tr").all("td").even.should(
+            have.exact_texts(
+                full_name,
+                email,
+                phone,
+                address,
+                date,
+                subject,
+                gender,
+                hobbies,
+                file,
+                state,
+            )
+        )
--- tests/test_demoqa_practice_form.py
+++ tests/test_demoqa_practice_form.py
@@ -1,68 +1,38 @@
-import os
-from selene import browser, be, have
+from pages.registration_page import RegistrationPage
 
 
-def test_demoqa_form():
-    browser.open("/automation-practice-form")
+def test_user_can_send_form():
+    registration_page = RegistrationPage()
+    (
+        (
+            registration_page.open_browser()
+            .fill_first_name('Yulia')
+            .fill_last_name('Vdovina')
+            .fill_email('yulya_vdovina@mail.ru')
+            .fill_number('0123456789')
+            .fill_address('Penza, st. Ladoga 1234')
+            .fill_date_of_birth('11', '028', '2000')
+            .fill_subjects('Economics')
+            .fill_gender('2')
+            .fill_hobbies('1')
+            .upload_file('kit.jpg')
+            .fill_state('Haryana')
+            .fill_city('Karnal')
 
-    # Заполнение текстовых полей
-    browser.element("#firstName").type("Yulia")
-    browser.element("#lastName").type("Vdovina")
-    browser.element("#userEmail").type("yulya_vdovina@mail.ru")
-    browser.element("#userNumber").type("0123456789")
-    browser.element("#currentAddress").type("Penza, st. Ladoga 1234")
-
-    # Установка даты (Date of Birth)
-    browser.element("#dateOfBirthInput").click()
-    browser.element('.react-datepicker__month-select option[value="11"]').click()
-    browser.element('.react-datepicker__year-select option[value="2000"]').click()
-    browser.all(".react-datepicker__day--028").second.click()
-
-    # Проверка поля subjects
-    browser.element("#subjectsInput").type("Social")
-    browser.element(".subjects-auto-complete__menu").click()
-    browser.element(
-        ".subjects-auto-complete__indicators .subjects-auto-complete__clear-indicator"
-    ).click()
-    browser.element("#subjectsInput").should(be.blank)
-
-    browser.element("#subjectsInput").type("Eco").press_tab()
-    # browser.element('.subjects-auto-complete__multi-value .subjects-auto-complete__multi-value__remove').click()
-
-    browser.element("#subjectsInput").type("En").press_tab()
-
-    # Радиокнопка (Gender)
-    browser.element('[for="gender-radio-2"]').click()
-
-    # Радиокнопка (Hobbies)
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[for="hobbies-checkbox-2"]').click().click()
-    browser.element('[for="hobbies-checkbox-3"]').click()
-
-    # Выбор файла (Picture)
-    browser.element(".form-file").click().element("#uploadPicture").send_keys(
-        os.path.abspath("data/kit.jpg")
-    )
-
-    # Выбор из выпадающего списка (State and City)
-    browser.element("#state").click().element("#react-select-3-option-2").click()
-    browser.element("#city").click().element("#react-select-4-option-0").click()
-
-    # Отправка
-    browser.element("#submit").click()
-
-    # Проверка
-    browser.element(".modal-content").element("table").all("tr").all("td").even.should(
-        have.exact_texts(
-            "Yulia Vdovina",
-            "yulya_vdovina@mail.ru",
-            "Female",
-            "0123456789",
-            "28 December,2000",
-            "Economics, English",
-            "Sports, Music",
-            "kit.jpg",
-            "Penza, st. Ladoga 1234",
-            "Haryana Karnal",
+            .submit()
+            .should_registered_user_with(
+                "Yulia Vdovina",
+                "yulya_vdovina@mail.ru",
+                "Female",
+                "0123456789",
+                "28 December,2000",
+                "Economics",
+                "Sports",
+                "kit.jpg",
+                "Penza, st. Ladoga 1234",
+                "Haryana Karnal",
+            )
         )
     )
+
+
